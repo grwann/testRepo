91af395096dae2ba0c24d092256763864ab77fd2
grwann <grwann@microsoft.com>
2017-05-24 09:15:26 -0700

    Create Sysprep.ps1

---
+++ sysprep/Sysprep.ps1
@@ -0,0 +1,8 @@
+exit 1
+
+# $newProcess = new-object System.Diagnostics.ProcessStartInfo "sysprep.exe" 
+# $newProcess.WorkingDirectory = "${env:SystemDrive}\windows\system32\sysprep" 
+# $newProcess.Arguments = "/generalize /oobe /shutdown" 
+# Write-Host $newProcess.Arguments 
+# $newProcess.Verb = "runas"; 
+# $process = [System.Diagnostics.Process]::Start($newProcess); 
