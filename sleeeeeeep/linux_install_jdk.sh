87e44e27b146769f6093a909a28894850d6df33a
grwann <grwann@microsoft.com>
2017-11-27 13:06:31 -0800

    Update linux_install_jdk.sh

--- sleeeeeeep/linux_install_jdk.sh
+++ sleeeeeeep/linux_install_jdk.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# script to install Java on Ubuntu (APT) systems or YUM-based systems.
-# 
-# Update the package system prior to installing Java to ensure we can get what we need.
-#
-# Version 0.1
-# Author Darren Rich <darrich@microsoft.com>
-# Original version found on VSTS: https://almrangers.visualstudio.com/
+# script to make artifact sleep
 
 echo "grace test"
 
