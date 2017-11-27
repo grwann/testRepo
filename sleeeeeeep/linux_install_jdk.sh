9d2b2d96d92cedf98109bfbf3a4b1e9fcf1b675d
grwann <grwann@microsoft.com>
2017-11-27 12:17:53 -0800

    Create linux_install_jdk.sh

---
+++ sleeeeeeep/linux_install_jdk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script to install Java on Ubuntu (APT) systems or YUM-based systems.
+# 
+# Update the package system prior to installing Java to ensure we can get what we need.
+#
+# Version 0.1
+# Author Darren Rich <darrich@microsoft.com>
+# Original version found on VSTS: https://almrangers.visualstudio.com/
+
+echo "grace test"
+
+sleep 5
+
+echo "done sleeping"
