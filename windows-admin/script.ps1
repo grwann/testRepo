c77f3d71dc0bea0cdf80e170678b5776000e4b50
grwann <grwann@microsoft.com>
2018-03-26 16:21:40 -0700

    Update script.ps1

--- windows-admin/script.ps1
+++ windows-admin/script.ps1
@@ -1,5 +1,11 @@
+Write-Output "starting"
+
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $text = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 
+Write-Output $text
+
 $UserPath = "Desktop\test.txt"
 $text > $UserPath
+
+Write-Output "done"
