38b6cecd1796d1e0fc86b642979c4273c7182530
grwann <grwann@microsoft.com>
2018-03-26 16:02:25 -0700

    Update script.ps1

--- windows-admin/script.ps1
+++ windows-admin/script.ps1
@@ -1,5 +1,5 @@
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $text = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 
-$UserPath = "%UserProfile%\Desktop\test.txt"
+$UserPath = "Desktop\test.txt"
 $text > $UserPath
