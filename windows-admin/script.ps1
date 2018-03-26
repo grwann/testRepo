5b5a0d11c77191a9bbef329f02327936a36302dc
grwann <grwann@microsoft.com>
2018-03-26 15:53:54 -0700

    Create script.ps1

---
+++ windows-admin/script.ps1
@@ -0,0 +1,2 @@
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
