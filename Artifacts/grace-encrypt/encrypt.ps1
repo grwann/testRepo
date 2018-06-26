1a3f261f26b1a792f9537dca6d5fedeba6f18ea3
grwann <grwann@microsoft.com>
2018-06-26 11:21:59 -0700

    Create encrypt.ps1

---
+++ Artifacts/grace-encrypt/encrypt.ps1
@@ -0,0 +1,22 @@
+Install-Module -name AzureRM -Scope CurrentUser
+
+$keyVaultRgName = "grace-rg"
+$keyVaultName = "encrypt-kv";
+$vmName = "encrypt-big02"
+$vmRgName = "grace-lab3759623017001"
+$key = "myKEK"
+$aadClientId = "da179a12-9f05-4cf3-aef7-c68187f2054f"
+$securePassword = ConvertTo-SecureString -String $aadPassword -AsPlainText -Force
+
+$diskEncryptionKeyVaultUrl = "https://encrypt-kv.vault.azure.net/";
+$keyVaultResourceId = "/subscriptions/e605a3bc-ee4e-4c7a-9709-1868a28b1d4d/resourceGroups/grace-rg/providers/Microsoft.KeyVault/vaults/encrypt-kv";
+$keyEncryptionKeyUrl = "https://encrypt-kv.vault.azure.net/keys/myKEK/1c2d74536b044750ae612e160a5aac0c"
+
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRgName `
+    -VMName $vmName `
+    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
+    -DiskEncryptionKeyVaultId $keyVaultResourceId `
+    -KeyEncryptionKeyUrl $keyEncryptionKeyUrl `
+    -KeyEncryptionKeyVaultId $keyVaultResourceId `
+    -AadClientID $aadClientId `
+    -AadClientSecret (New-Object PSCredential "user",$securePassword).GetNetworkCredential().Password
