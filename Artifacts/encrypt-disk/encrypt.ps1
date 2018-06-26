a11cc32ec3f3e07e859074457ef484ed2830b4d4
grwann <grwann@microsoft.com>
2018-06-26 07:30:59 -0700

    Update encrypt.ps1

--- Artifacts/encrypt-disk/encrypt.ps1
+++ Artifacts/encrypt-disk/encrypt.ps1
@@ -1,20 +1,41 @@
-$keyVaultRgName = "grace-rg"
-$keyVaultName = "grace-encrypt";
-$vmName = "test-encrypt4"
-$vmRgName = "grace-lab3759623017001"
-$key = "encrypt-key"
-$securePassword = ConvertTo-SecureString -String $vmPassword -AsPlainText -Force
-
-$keyVault = Get-AzureRmKeyVault -VaultName $keyVaultName -ResourceGroupName $keyVaultRgName;
-$diskEncryptionKeyVaultUrl = $keyVault.VaultUri;
-$keyVaultResourceId = $keyVault.ResourceId;
-$keyEncryptionKeyUrl = (Get-AzureKeyVaultKey -VaultName $keyVaultName -Name $key).Key.kid;
-
-Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRgName `
-    -VMName $vmName `
-    -AadClientID $app.ApplicationId `
-    -AadClientSecret (New-Object PSCredential "user",$securePassword).GetNetworkCredential().Password `
-    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
-    -DiskEncryptionKeyVaultId $keyVaultResourceId `
-    -KeyEncryptionKeyUrl $keyEncryptionKeyUrl `
-    -KeyEncryptionKeyVaultId $keyVaultResourceId
+{
+    "$schema": "https://raw.githubusercontent.com/Azure/azure-devtestlab/master/schemas/2016-11-28/dtlArtifacts.json",
+    "title": "CPU - Windows - Run Azure Disk Encryption extension",
+    "description": "Runs the AzureDiskEncryption extension to encrypt the disks on the VM.",
+    "tags": [
+        "Windows"
+    ],
+    "targetOsType": "Windows",
+    "parameters": {
+        "appId": {
+            "type": "string",
+            "displayName": "AAD Client Id",
+            "description": "The azure credential to use"
+        },
+        "password": {
+            "type": "securestring",
+            "displayName": "Password",
+            "description": "The password for the azure credential"
+        },
+        "keyVaultURL": {
+            "type": "string",
+            "displayName": "Key Vault URL",
+            "description": "Key Vault URL e.g https://keyvaultname.vault.azure.net"
+        }
+    },
+    "runAzureVMExtension": {
+        "publisher": "Microsoft.Azure.Security",
+        "type": "AzureDiskEncryption",
+        "typeHandlerVersion": "1.1",
+        "autoUpgradeMinorVersion": false,
+        "settings": {
+            "AADClientID": "[parameters('appId')]",
+            "KeyVaultURL": "[parameters('keyVaultURL')]",
+            "EncryptionOperation": "EnableEncryption",
+            "VolumeType":"All"
+        },
+        "protectedSettings": {
+            "AADClientSecret": "[parameters('password')]"
+        }
+    }
+}
