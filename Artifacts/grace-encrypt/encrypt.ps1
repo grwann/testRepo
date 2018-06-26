7e2f82b027e21990e84a1e5b8831541c5864fb25
grwann <grwann@microsoft.com>
2018-06-26 13:27:07 -0700

    Update encrypt.ps1

--- Artifacts/grace-encrypt/encrypt.ps1
+++ Artifacts/grace-encrypt/encrypt.ps1
@@ -1,20 +1,22 @@
-Install-Module -name AzureRM -Scope CurrentUser
+Write-Output $aadPassword
 
-$keyVaultRgName = "grace-rg"
-$keyVaultName = "encrypt-kv";
-$vmName = "encrypt-big03"
-$vmRgName = "grace-lab3759623017001"
-$key = "myKEK"
-$aadClientId = "da179a12-9f05-4cf3-aef7-c68187f2054f"
-$diskEncryptionKeyVaultUrl = "https://encrypt-kv.vault.azure.net/";
-$keyVaultResourceId = "/subscriptions/e605a3bc-ee4e-4c7a-9709-1868a28b1d4d/resourceGroups/grace-rg/providers/Microsoft.KeyVault/vaults/encrypt-kv";
-$keyEncryptionKeyUrl = "https://encrypt-kv.vault.azure.net/keys/myKEK/1c2d74536b044750ae612e160a5aac0c"
+#Install-Module -name AzureRM -Scope CurrentUser
 
-Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRgName `
-    -VMName $vmName `
-    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
-    -DiskEncryptionKeyVaultId $keyVaultResourceId `
-    -KeyEncryptionKeyUrl $keyEncryptionKeyUrl `
-    -KeyEncryptionKeyVaultId $keyVaultResourceId `
-    -AadClientID $aadClientId `
-    -AadClientSecret $aadPassword
+#$keyVaultRgName = "grace-rg"
+#$keyVaultName = "encrypt-kv";
+#$vmName = "encrypt-big03"
+#$vmRgName = "grace-lab3759623017001"
+#$key = "myKEK"
+#$aadClientId = "da179a12-9f05-4cf3-aef7-c68187f2054f"
+#$diskEncryptionKeyVaultUrl = "https://encrypt-kv.vault.azure.net/";
+#$keyVaultResourceId = "/subscriptions/e605a3bc-ee4e-4c7a-9709-1868a28b1d4d/resourceGroups/grace-rg/providers/Microsoft.KeyVault/vaults/encrypt-kv";
+#$keyEncryptionKeyUrl = "https://encrypt-kv.vault.azure.net/keys/myKEK/1c2d74536b044750ae612e160a5aac0c"
+
+#Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRgName `
+#    -VMName $vmName `
+#    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
+#    -DiskEncryptionKeyVaultId $keyVaultResourceId `
+#    -KeyEncryptionKeyUrl $keyEncryptionKeyUrl `
+#    -KeyEncryptionKeyVaultId $keyVaultResourceId `
+#    -AadClientID $aadClientId `
+#    -AadClientSecret $aadPassword
