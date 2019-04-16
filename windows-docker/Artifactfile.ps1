4966088eeafe2b914139fc1c19892f32d45c4b24
grwann <grwann@microsoft.com>
2019-04-16 09:45:48 -0700

    Update Artifactfile.ps1

--- windows-docker/Artifactfile.ps1
+++ windows-docker/Artifactfile.ps1
@@ -314,6 +314,8 @@ try
 
             if ((Test-Path -Path $dockerPath -PathType Container) -and (Test-Path -Path $kitematicPath -PathType Container)) {
 
+                Write-Output "kitematicPath:" $kitematicPath
+                Write-Output "dockerPath:" $dockerPath
                 # redirect default kitematic folder under docker to the chocolatey package folder 
                 New-Item -Path (Join-Path $dockerPath "Kitematic") -ItemType SymbolicLink -Target $kitematicPath | Out-Null
             }
