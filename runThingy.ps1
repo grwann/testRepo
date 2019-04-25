12ac148f0988e105ec7d1410d0031e56c695d75d
grwann <grwann@microsoft.com>
2019-04-24 17:55:27 -0700

    Update runThingy.ps1

--- runThingy.ps1
+++ runThingy.ps1
@@ -3,17 +3,7 @@ Param(
 	[string]$accountKey
 	)
 
-$connectFile = "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ConnectShare.cmd"
+$cmd = "net use z: \\$accountName.file.core.windows.net\share /u:$accountName $accountKey"
+$cmd | Set-Content "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ConnectShare.cmd"
 
-if (!(Test-Path $connectFile -PathType Leaf)) {
-
-net use * /delete
-$cmd = "net use x: \\$accountName.file.core.windows.net\scratch /u:$accountName $accountKey"
-$cmd | Add-Content $connectFile
-
-#New-ItemProperty -Path 'HKLM:SystemCurrentControlSetControlTerminal Server' -Name 'fDenyTSConnections' -Value 0 -PropertyType dword -Force
-#New-ItemProperty -Path 'HKLM:SystemCurrentControlSetControlTerminal ServerWinStationsRDP-Tcp' -Name 'UserAuthentication' -Value 1 -PropertyType dword -Force
-
-#Restart-Computer -Force -AsJob
-
-}
+Restart-Computer -Force
