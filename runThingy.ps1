20a3b9168b0993d261455e58e88cd922caa1a0b6
grwann <grwann@microsoft.com>
2019-04-24 18:03:10 -0700

    Update runThingy.ps1

--- runThingy.ps1
+++ runThingy.ps1
@@ -3,7 +3,7 @@ Param(
 	[string]$accountKey
 	)
 
-$cmd = "net use z: \\$accountName.file.core.windows.net\share /u:$accountName $accountKey"
+$cmd = "net use z: \\$accountName.file.core.windows.net\scratch /u:$accountName $accountKey"
 $cmd | Set-Content "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ConnectShare.cmd"
 
 Restart-Computer -Force
